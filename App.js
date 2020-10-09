b9d95d404a2a36bbb10025de41b645de9bd09a4e
Bharti Parmar <38555462+Bharti-Parmar@users.noreply.github.com>
2020-10-09 01:44:30 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Index from './ReusableCard/components/index';
+import Index from './ReusableCounter/index.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
+}
+
+export default App;
